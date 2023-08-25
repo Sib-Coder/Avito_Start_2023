74787bd03bd6a4d500ae6f3f0bf8b6d7cf711c6e
Sinitsyn Daniil <80570939+Sib-Coder@users.noreply.github.com>
2023-08-25 16:10:59 +0700

    add start database script

---
+++ DataBase/createscript.sql
@@ -0,0 +1,26 @@
+create table users
+(
+    id_user         UUID primary key default gen_random_uuid(),
+    name       varchar(100) not null,
+    same_info varchar(100)
+
+) ;
+
+create table slug
+(
+    id_slug        UUID primary key default gen_random_uuid(),
+    name_slug      varchar(100) not null
+
+) ;
+
+create table slugtraker
+(
+    id       UUID primary key default gen_random_uuid(),
+    id_user uuid  REFERENCES users (id_user),
+    id_slug uuid  REFERENCES slug (id_slug)
+
+) ;
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Avito_Start_2023
+тестовое задание для Авито https://github.com/avito-tech/backend-trainee-assignment-2023
